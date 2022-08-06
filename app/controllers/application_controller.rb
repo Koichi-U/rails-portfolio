58de371dd265b0d577b3aa3569ab8e573c9e6dc1
Koichi-U <valleyvalley123@icloud.com>
2022-08-06 22:05:12 +0900

    Set a post function in header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_tag
+  
+  def set_tag
+    @tags = Tag.all
+  end
 
   protected
   def configure_permitted_parameters
