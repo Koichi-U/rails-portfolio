7b5fbf83de08ddeb1e6448045146ee331cbc51ef
Koichi-U <valleyvalley123@icloud.com>
2022-08-15 05:16:09 +0900

    Avoid error when sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :set_tag
   
   def set_tag
-    @tags = Tag.joins(:user).where(user: { admin: true }).or(Tag.joins(:user).where(user_id: current_user.id)).order("name")
+    if user_signed_in?
+      @tags = Tag.joins(:user).where(user: { admin: true }).or(Tag.joins(:user).where(user_id: current_user.id)).order("name")
+    else
+      @tags = Tag.joins(:user).where(user: { admin: true })
+    end
   end
 
   protected
